93f6bcd1c973559d5a84ee04ce4834a59dc18dc3
Victor Oliveira <victoroliveira.dev@gmail.com>
2023-10-29 09:49:47 -0300

    feat: atualizações do professor

--- src/main/java/br/edu/infnet/appvenda/model/domain/Vendedor.java
+++ src/main/java/br/edu/infnet/appvenda/model/domain/Vendedor.java
@@ -2,10 +2,23 @@ package br.edu.infnet.appvenda.model.domain;
 
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "TVendedor")
 public class Vendedor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String nome;
 	private String cpf;
 	private String email;
+	@Transient
 	private List<Produto> produtos;
 
 	@Override
