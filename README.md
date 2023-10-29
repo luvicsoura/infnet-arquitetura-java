09dcf0eebec03aa6dd30d8db65f87492936469c9
Victor Oliveira <victoroliveira.dev@gmail.com>
2023-10-29 13:46:07 -0300

    fix: database dependencies

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 **Java**: OpenJDK 21 (https://jdk.java.net)<br/>
 **SpringBoot**: 3.1.5
+
+## Instruções
+
+Antes de rodar o projeto, garanta que tenha um banco de dados MySQL rodando na porta 3306 com o seguinte usuário:
+
+```
+	usuário: arqjava
+	senha: arqjavapwd
+```
--- src/main/java/br/edu/infnet/appvenda/model/domain/Vendedor.java
+++ src/main/java/br/edu/infnet/appvenda/model/domain/Vendedor.java
@@ -2,12 +2,12 @@ package br.edu.infnet.appvenda.model.domain;
 
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "TVendedor")
